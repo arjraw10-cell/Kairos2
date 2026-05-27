3113855ed74c7167951f093b8496058ab012c8c7
arjraw10-cell <arjraw10@gmail.com>
2026-05-26 23:49:01 -0700

    Fix python path import issue

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import sys
 import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import sys
+import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 import asyncio
 from textual.app import App as TextualApp, ComposeResult
